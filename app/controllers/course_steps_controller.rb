b11e153239d824cc6b22f884379f4ca479d47448
Chaipat Suwannaphum <chaipatsuwannaphum@gmail.com>
2018-04-02 15:06:15 +0700

    update database to be postgresql

--- app/controllers/course_steps_controller.rb
+++ app/controllers/course_steps_controller.rb
@@ -1,7 +1,7 @@
 class CourseStepsController < ApplicationController
   skip_before_action :verify_authenticity_token
   include Wicked::Wizard
-  steps :about_course, :topics, :instructors, :course_meta, :objectives, :targets, :problems_solutions, :chapters, :outcome, :evaluation
+  steps :about_course, :topics, :instructors, :course_meta, :objectives, :targets, :problems_solutions, :chapters, :evaluation
 
   def show
 
@@ -43,7 +43,7 @@ class CourseStepsController < ApplicationController
     def course_params
       params.require(:course).permit(:courseName, :about_course, :length,
                                      :effort, :price, :institution, :subject, :level, :languages, :evaluation,
-                                     :videots, :prerequisites, :avatar, :status, :category_id,:outcome,
+                                     :videots, :prerequisites, :avatar, :status, :category_id, :outcome,
                                      topics_attributes: [:id, :name, :_destroy],
                                      instructors_attributes: [:id, :name, :avatar, :_destroy],
                                      targets_attributes: [:id, :name, :_destroy],
